a44b76731ff18c2a4b0ff2193521f64a63d29332
brandon_es <bruno111937@gmail.com>
2024-11-26 22:10:27 +0800

    main-menu-split

--- egg_roll.py
+++ egg_roll.py
@@ -485,125 +485,35 @@ def game_state(level_file):
     #End of game_state()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-def menu():
-    '''
-    This is the game's MAIN MENU.
-    '''
-    while True:
-        try:
-            #This lists ALL of the contents of the ./levels folder valid for egg_roll or not
-            level_list = os.listdir("./levels")
-            if debug:
-                print("# level_list initialized")
-        except FileNotFoundError:
-            print('Sorry but please run the application from the egg_roll folder!')
-            print()
-            return
-
-        #Gameplay initialization
-        if not level_list and len(sys.argv) < 2:
-            #End program if no level_file can be loaded
-            print("<Welcome to EGG ROLL!>")
-            print()
-            print('No level files available...')
-            print('Please restart the game with a valid file location argument or populated ./levels folder')
-            print()
-            return None
-
-        elif len(sys.argv) == 2:
-            #This program prioritizes level_file argument over Level Selection
-            while True:
-                with open(sys.argv[1], encoding='utf-8') as level_file:
+def argument_handling():
+    #Gameplay initialization
+    print("<Welcome to EGG ROLL!>")
+    print()
+    try:
+        while True:
+            with open(sys.argv[1], encoding='utf-8') as level_file:
+                try:
+                    game_state(level_file)
+                except FileNotFoundError:
+                    #End program if argument level_file invalid
                     try:
-                        game_state(level_file)
-                    except FileNotFoundError:
-                        #End program if argument level_file invalid
-
-                        try:
-                            clear_screen(debug)
-                        except NameError:
-                            print("# clear_screen(debug)")
-
-                        print("<Welcome to EGG ROLL!>")
-                        print()
-                        print("File argument invalid! Please open game with valid file location...")
-                        print()
-                        return
-                    repeat = input("Type [Yes] to replay level, else go back to main menu: ")
-                    if repeat.lower == 'yes':
-                        continue
-                    else:
-                        break
-                break
-
-        else:
-            while True:
-
-                try:
-                    clear_screen(debug)
-                except NameError:
-                    print("# clear_screen(debug)")
-
-                print(
-                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
-                    "",
-                    " _|_|_|_/                        _|_|_|_\\            _|    _|",
-                    " _|          _|_|\\     _|_|\\     _|     _|   ▓▓▒░░   _|    _|",
-                    " _|_|_|    _/    _|  _/    _|    _|_|_|_/   ▓▓▒▒▒▒░  _|    _|",
-                    " _|        _\\    _|  _\\    _|    _|    _\\   █▓▓▒▒▒░  _|    _|",
-                    " _|_|_|_\\    _|_|_|    _|_|_|    _|     _\\   █▓▓▒░   _|_\\  _|_\\",
-                    "                  |         |",
-                    "             _|_|/     _|_|/",
-                    "",
-                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
-                    sep='\n')
-                print()
-
-                print("<Level Selection>")
-                for i in range(1, len(level_list)+1):
-                    print(f"{i}. {level_list[i-1]}")
-                print()
-                
-                print("Type [Quit] to exit program or")
-                try:
-                    choice = input(colored("Enter a valid level_file.in:", attrs=["reverse"]) + " ")
-                except NameError:
-                    choice = input("Enter a valid level_file.in: ")
-
-                if choice.lower() == "quit":
-                    break
-                elif choice not in level_list:
-                    print("Invalid level_file, please try again...")
-                    time.sleep(2)
+                        clear_screen(debug)
+                    except NameError:
+                        print("# clear_screen(debug)")
+                    print("File argument invalid! Please open game with valid file location...")
+                    print()
+                    return
+                repeat = input("Type [Yes] to replay level, else exit game: ")
+                if repeat.lower() == 'yes':
                     continue
                 else:
-                    pass
-                
-                while True:
-                    with open("./levels/" + choice, encoding='utf-8') as level_file:
-                        game_state(level_file)
-                        repeat = input("Type [Yes] to replay level, else go back to main menu: ")
-                        if repeat.lower() == 'yes':
-                            continue
-                        else:
-                            try:
-                                clear_screen(debug)
-                            except NameError:
-                                print("# clear_screen(debug)")
-                            break
                     break
-        
-        while True:
-            quit = input("Really quit EGG ROLL [Yes]? ")
-            if quit.lower() == "yes":
-                print()
-                print("<Thank you for playing egg_roll.py by Martin Mendoza (2024-10322) & Brandon Sayo (2024-05352)!>")
-                print()
-                return None
-            else:
-                pass
-            break
-        continue
+        print()
+        print("<Thank you for playing egg_roll.py by Martin Mendoza (2024-10322) & Brandon Sayo (2024-05352)!>")
+        print()
+
+    except:
+        print("No file argument! Please open file with valid file location argument...")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Program initialization
@@ -627,4 +537,4 @@ except ImportError:
 
 #This is to prevent menu from running when imported for unit testing
 if __name__ == '__main__':
-    menu()
+    argument_handling()
---
+++ menu.py
@@ -0,0 +1,148 @@
+import os
+import sys
+import time
+
+from egg_roll import Level, clear_screen, game_state
+
+def menu():
+    '''
+    This is the game's MAIN MENU.
+    '''
+    while True:
+        try:
+            #This lists ALL of the contents of the ./levels folder valid for egg_roll or not
+            level_list = os.listdir("./levels")
+            if debug:
+                print("# level_list initialized")
+        except FileNotFoundError:
+            print('Sorry but please run the application from the egg_roll folder!')
+            print()
+            return
+
+        #Gameplay initialization
+        if not level_list and len(sys.argv) < 2:
+            #End program if no level_file can be loaded
+            print("<Welcome to EGG ROLL!>")
+            print()
+            print('No level files available...')
+            print('Please restart the game with a valid file location argument or populated ./levels folder')
+            print()
+            return None
+
+        elif len(sys.argv) == 2:
+            #This program prioritizes level_file argument over Level Selection
+            with open(sys.argv[1], encoding='utf-8') as level_file:
+                while True:
+                    try:
+                        game_state(level_file)
+                    except FileNotFoundError:
+                        #End program if argument level_file invalid
+
+                        try:
+                            clear_screen(debug)
+                        except NameError:
+                            print("# clear_screen(debug)")
+
+                        print("<Welcome to EGG ROLL!>")
+                        print()
+                        print("File argument invalid! Please open game with valid file location...")
+                        print()
+                        return
+                    repeat = input("Type [Yes] to replay level, else go back to main menu: ")
+                    if repeat.lower() == 'yes':
+                        continue
+                    else:
+                        break
+
+        else:
+            while True:
+
+                try:
+                    clear_screen(debug)
+                except NameError:
+                    print("# clear_screen(debug)")
+
+                print(
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+                    "",
+                    " _|_|_|_/                        _|_|_|_\\            _|    _|",
+                    " _|          _|_|\\     _|_|\\     _|     _|   ▓▓▒░░   _|    _|",
+                    " _|_|_|    _/    _|  _/    _|    _|_|_|_/   ▓▓▒▒▒▒░  _|    _|",
+                    " _|        _\\    _|  _\\    _|    _|    _\\   █▓▓▒▒▒░  _|    _|",
+                    " _|_|_|_\\    _|_|_|    _|_|_|    _|     _\\   █▓▓▒░   _|_\\  _|_\\",
+                    "                  |         |",
+                    "             _|_|/     _|_|/",
+                    "",
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+                    sep='\n')
+                print()
+
+                print("<Level Selection>")
+                for i in range(1, len(level_list)+1):
+                    print(f"{i}. {level_list[i-1]}")
+                print()
+                
+                print("Type [Quit] to exit program or")
+                try:
+                    choice = input(colored("Enter a valid level_file.in:", attrs=["reverse"]) + " ")
+                except NameError:
+                    choice = input("Enter a valid level_file.in: ")
+
+                if choice.lower() == "quit":
+                    break
+                elif choice not in level_list:
+                    print("Invalid level_file, please try again...")
+                    time.sleep(2)
+                    continue
+                else:
+                    pass
+                
+                while True:
+                    with open("./levels/" + choice, encoding='utf-8') as level_file:
+                        game_state(level_file)
+                        repeat = input("Type [Yes] to replay level, else go back to main menu: ")
+                        if repeat.lower() == 'yes':
+                            continue
+                        else:
+                            try:
+                                clear_screen(debug)
+                            except NameError:
+                                print("# clear_screen(debug)")
+                            break
+                    break
+        
+        while True:
+            quit = input("Really quit EGG ROLL [Yes]? ")
+            if quit.lower() == "yes":
+                print()
+                print("<Thank you for playing egg_roll.py by Martin Mendoza (2024-10322) & Brandon Sayo (2024-05352)!>")
+                print()
+                return None
+            else:
+                pass
+            break
+        continue
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#Program initialization
+
+global debug, text_based
+debug = False #This toggles insider info print and disables clear_screen calls
+text_based = False #This disables emojis and utilizes ASCII instead (WARNING!: REQUIRES ASCII-BASED LEVEL_FILE)
+
+if debug:
+    print("~~~~~~~~~~~~~~~~~")
+    print("# MAIN MENU STARTED: DEBUG IS ON")
+
+try:
+    from termcolor import colored, cprint
+    if debug:
+        print("# termcolor loaded")
+except ImportError:
+    if debug:
+        print("# termcolor NOT loaded")
+    pass
+
+#This is to prevent menu from running when imported for unit testing
+if __name__ == '__main__':
+    menu()
