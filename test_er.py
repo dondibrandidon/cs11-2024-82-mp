32d87dc03c1bed98e4acfb448262c7e48a0c4f0d
brandon_es <bruno111937@gmail.com>
2024-12-06 23:20:16 +0800

    day one patch pt2

--- test_er.py
+++ test_er.py
@@ -9,7 +9,7 @@ from egg_roll import Level, game_state
 test_level = Level(grid, moves_left)
 
 if character in 'fFbBrRlL':
-    new_grid, increment_points, animation, no_eggs = current_level.tilt(character, moves_left)
+    increment_points, animation, no_eggs = current_level.tilt(character, moves_left)
 
 file_name = valid_location/level_file.in
 with open(file_name, encoding='utf-8') as level_file:
@@ -18,7 +18,7 @@ with open(file_name, encoding='utf-8') as level_file:
 
 #-------------------------------------------START OF TESTS------------------------------------------------#
 
-# Expected file format for _test_Level_Tilt (".|unit_testing|test_Level_Tilt|file.in"):
+# Expected file format for _test_Level_tilt (".|unit_testing|test_Level_tilt|file.in"):
 '''
 level_rows: int
 moves_left: int
@@ -49,9 +49,9 @@ def test_Level_tilt(test_file):
         expected_points = int(level_file.readline())
         expected_no_eggs = int(level_file.readline())
 
-        new_grid, increment_points, animation, no_eggs = test_level.tilt(character_input, moves_left)
+        increment_points, animation, no_eggs = test_level.tilt(character_input, moves_left)
 
-        assert fin_grid == new_grid
+        assert fin_grid == test_level.get_grid()
         assert expected_points == increment_points
         assert expected_no_eggs == no_eggs
 
